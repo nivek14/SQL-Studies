ad8d01036d844322d8d33a9c98a1cf9eedb631d3
prichuad <kevinoliveiracc14@gmail.com>
2023-07-20 19:55:06 -0300

    Create db_basic.sql

---
+++ SQL Bootcamp/Section 3/db_basic.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE brasileirao;
+
+SHOW DATABASES;
+
+USE brasileirao;
+
+SELECT DATABASE();
+
+CREATE TABLE tbl_clube(
+Idclube SMALLINT AUTO_INCREMENT,
+NomeClube varchar(60) NOT NULL,
+CONSTRAINT pk_id_time PRIMARY KEY (Idclube)
+);
+
+CREATE TABLE tbl_time(
+IdJogador SMALLINT AUTO_INCREMENT,
+Nomejogador varchar(60) NOT NULL,
+SobrenomeJogador VARCHAR(60) NOT NULL,
+Assistencias SMALLINT,
+Gols SMALLINT,
+CONSTRAINT pk_id_jogador PRIMARY KEY (IdJogador)
+);
+
+CREATE TABLE tbl_comissao_tecnica(
+IdMembro SMALLINT AUTO_INCREMENT,
+NomeMembro varchar(60) NOT NULL,
+SobrenomeMembro VARCHAR(60) NOT NULL,
+Cargo VARCHAR(60) NOT NULL,
+CONSTRAINT pk_id_jogador PRIMARY KEY (IdMembro)
+);
+
+
